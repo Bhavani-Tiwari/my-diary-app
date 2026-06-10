47ae9b6ea7c1f6e589b27594f100421ceb022226
Bhavani Tiwari <kunnatiwari6@gmail.com>
2026-06-10 19:24:39 +0530

    Initial MyDiary project upload

---
+++ src/main/java/com/org/spring/mydiary/business/EntryBusinessInterface.java
@@ -0,0 +1,18 @@
+package com.org.spring.mydiary.business;
+
+import java.util.List;
+
+import com.org.spring.mydiary.entities.Entry;
+
+public interface EntryBusinessInterface {
+
+
+	public void save(Entry entry);
+	public void update(Entry entry);
+	public void delete(Entry entry);
+	public Entry findById(int id);
+	public List<Entry> findAll();
+	public List<Entry> findByUserid(int id);
+
+	
+}
